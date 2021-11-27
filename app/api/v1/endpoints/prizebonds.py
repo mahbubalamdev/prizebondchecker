59fd95700416b90b9c9b154c984f950162a30201
Mahbub Alam <mahbub.alam@britecore.com>
2021-11-27 08:56:00 +0600

    Use dynamo table to put item

--- app/api/v1/endpoints/prizebonds.py
+++ app/api/v1/endpoints/prizebonds.py
@@ -6,20 +6,19 @@ from boto3.dynamodb.conditions import Key
 
 
 prize_bond_table_name = os.getenv("PRIZE_BOND_TABLE")
-
+dynamodb = boto3.resource('dynamodb')
+prize_bond_table = dynamodb.Table(prize_bond_table_name)
 
 def get_username(request: Request):
     token = request.headers["authorization"].replace('Bearer ', '')
     return jwt.decode(token, options={"verify_signature": False})["username"]
 
-dynamo_client = boto3.client('dynamodb')
-dynamodb = boto3.resource('dynamodb')
 router = APIRouter()
 
 @router.get('/')
 def get_prize_bonds(request: Request):
     username = get_username(request)
-    prize_bond_table = dynamodb.Table(prize_bond_table_name)
+    
     response = prize_bond_table.query(
         KeyConditionExpression=Key('Username').eq(username)
     )
@@ -27,18 +26,25 @@ def get_prize_bonds(request: Request):
 
 
     
-@router.post("/add/")
+@router.post("/")
 async def add_prize_bond(request: Request):
     username = get_username(request)
     request_body = await request.json()
     prize_bond_number =request_body["number"]
-    dynamo_client.put_item(
+    prize_bond_table.put_item(
     TableName=prize_bond_table_name,
     Item={
-        'BondNumber': {
-            'S': prize_bond_number,
-        },
-        'Username': {
-            'S': username,
-        }
+        'BondNumber': prize_bond_number,
+        'Username': username
+    })
+
+@router.delete("/")
+async def delete_prize_bond(request: Request):
+    username = get_username(request)
+    request_body = await request.json()
+    prize_bond_number =request_body["number"]
+    prize_bond_table.delete_item(
+    Key={
+        'BondNumber': prize_bond_number,
+        'Username': username
     })
